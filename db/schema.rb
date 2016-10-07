df3c48d8ffda5ffd884e196ac67b5070d4e8df6f
Lee-KangChen <nihaifong@gmail.com>
2016-10-07 21:29:20 +0800

    add remove user table column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006115754) do
+ActiveRecord::Schema.define(version: 20161007132258) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -44,8 +44,9 @@ ActiveRecord::Schema.define(version: 20161006115754) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
