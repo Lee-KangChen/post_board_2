e27142912b12af04c0eb317b9eb64878efe58dad
Lee-KangChen <nihaifong@gmail.com>
2016-10-08 13:13:51 +0800

    added current user and logged_in helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:name])
 
-    if user && user.authenicate(params[:password])
-      sessions[:user_id] = user.id
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
       flash[:notice] = "You've logged in!"
       redirect_to root_path
     else
